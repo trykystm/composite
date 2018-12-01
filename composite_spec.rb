e29cd2698a04aafe1f90ab4dc49679fb5a8c0458
trykystm <trykystm@gmail.com>
2018-12-01 13:52:40 +0900

    Update composite_spec.rb

--- composite_spec.rb
+++ composite_spec.rb
@@ -1,3 +1,10 @@
 require './composite'
 RSpec.describe Composite do
+  subject do
+    composite = Composite.new
+    composite << 'a'
+    composite << ['b', 'c']
+  end
+  xdescribe '#each' do
+  end
 end
